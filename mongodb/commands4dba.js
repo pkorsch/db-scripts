6992740ade60d129b742171b8302fd5f2c063973
pkorsch <2014609+pkorsch@users.noreply.github.com>
2021-01-04 14:15:32 +0100

    Update commands4dba.js

--- mongodb/commands4dba.js
+++ mongodb/commands4dba.js
@@ -1,48 +1,61 @@
---change database
+// change database
 use admin
 
--- show all granted databases
+// show all granted databases
 show dbs
 db.adminCommand( { listDatabases: 1 } )
 
--- database statistics
+// database statistics
 db.stats()
 
--- all collection info or specific db.getCollectionInfos( { name: "employees" } )  
+// all collection info or specific db.getCollectionInfos( { name: "employees" } )  
 use DATABASE_NAME
 db.getCollectionInfos();
 
--- show all db parameters
+// show all db parameters
 db.adminCommand( { getParameter : '*' } )
 
--- get indexes for collection
+// get indexes for collection
 db.collection.getIndexes()
 
--- show profiling level
+// show profiling level
 db.getProfilingStatus()
 
--- show users or specific user ex. db.getUser("arminUser")
+// show users or specific user ex. db.getUser("arminUser")
 show users
 db.getUsers()
 
--- switch logfile
+// switch logfile
 db.adminCommand( { logRotate : 1 } )
 
--- current operations
+// current operations
 db.currentOp()
 
--- kill operation
+// kill operation
 db.adminCommand( { "killOp": 1, "op": operation_id } )
 
--- current replicaset configuration
+// current replicaset configuration
 rs.conf()
 
--- current status of replicaset
+// current status of replicaset
 rs.status()
 
--- replication lag
+// replication lag
 rs.printSlaveReplicationInfo()
 
--- shutdown now or with timer
+// shutdown now or with timer
 db.shutdownServer()
 db.adminCommand({ "shutdown" : 1, timeoutSecs: 60 })
+
+// server status
+db.serverStatus().host
+db.serverStatus().version
+db.serverStatus().uptime
+db.serverStatus().proccess
+db.serverStatus().pid
+db.serverStatus().localTime
+db.serverStatus().connections
+db.serverStatus().asserts
+db.serverStatus().locks
+db.serverStatus().opcounters
+db.serverStatus().opcounters.inserts
